63b0b8f821d09f3e05e05405787e1cbd5d25db6a
Leandro Souto de Paula <le-soutopaula@hotmail.com>
2024-10-12 22:47:07 -0300

    mobile tweaks

--- src/app/character/character.module.ts
+++ src/app/character/character.module.ts
@@ -5,9 +5,10 @@ import { CharacterRoutingModule } from './character-routing.module';
 import { TranslocoModule, provideTranslocoScope } from '@ngneat/transloco';
 import { CharacterComponent } from './character.component';
 import { FormsModule } from '@angular/forms';
+import { AttributeModalComponent } from '../attribute-modal/attribute-modal.component';
 
 @NgModule({
-  declarations: [CharacterComponent],
+  declarations: [CharacterComponent, AttributeModalComponent],
   imports: [
     CommonModule,
     CharacterRoutingModule,
